286a07f6e80b1cb24913d99eca5aa2c53ec584a0
Spik8 <spike123on@gmail.com>
2018-03-15 09:52:32 +0100

    Report plus

--- app/Http/Controllers/RaportsController.php
+++ app/Http/Controllers/RaportsController.php
@@ -569,15 +569,29 @@ class RaportsController extends Controller
     function setSingleRaportPlus($date,$typ)
     {
 ///////////////////////////////////////BADANIA Wysylka/////////////////////////////////////////////
+        $resandship = DB::table('log_download')
+            ->selectRaw('
+                    sum(baza8) as bisnode,
+                    sum(bazazg) as zgody,
+                    sum(bazareszta) as reszta,
+                    sum(bazaevent) as event,
+                    sum(bazaexito) as exito,
+                    
+                    sum(baza8Zgody) as bisnodeZgody,
+                    sum(bazazgZgody) as zgodyZgody,
+                    sum(bazaresztaZgody) as resztaZgody,
+                    sum(bazaeventZgody) as eventZgody,
+                    sum(bazaexitoZgody) as exitoZgody,
+                                        
+                    sum(baza8)+sum(bazazg)+sum(bazareszta)+sum(bazaevent)+sum(bazaexito)
+                     + sum(baza8Zgody)+sum(bazazgZgody)+sum(bazaresztaZgody)+sum(bazaeventZgody)+sum(bazaexitoZgody) as suma');
         if($typ == 1) {
-            $resandship = DB::table('log_download')
-                ->selectRaw('sum(baza8) as bisnode,sum(bazazg) as zgody,sum(bazareszta) as reszta,sum(bazaevent) as event,sum(bazaexito) as exito,sum(baza8)+sum(bazazg)+sum(bazareszta)+sum(bazaevent)+sum(bazaexito) as suma')
+            $resandship = $resandship
                 ->where('id_user', '>', 100)
                 ->where('date', 'like', $date[0] . '%')
                 ->get();
         }else {
-            $resandship = DB::table('log_download')
-                ->selectRaw('sum(baza8) as bisnode,sum(bazazg) as zgody,sum(bazareszta) as reszta,sum(bazaevent) as event,sum(bazaexito) as exito,sum(baza8)+sum(bazazg)+sum(bazareszta)+sum(bazaevent)+sum(bazaexito) as suma')
+            $resandship = $resandship
                 ->where('id_user', '>', 100)
                 ->whereBetween('date', [$date[0].'%',$date[1].' 23:59:59'])
                 ->get();
@@ -586,21 +600,36 @@ class RaportsController extends Controller
         $resandship = $this->setArray($resandship);
 
 ///////////////////////////////////////BADANIA Oddziały/////////////////////////////////////////////
+        $departamentres = DB::table('log_download')
+            ->selectRaw('
+                departments_t.name,
+                departments_t.id,
+                    sum(baza8) as bisnode,
+                    sum(bazazg) as zgody,
+                    sum(bazareszta) as reszta,
+                    sum(bazaevent) as event,
+                    sum(bazaexito) as exito,
+                    
+                    sum(baza8Zgody) as bisnodeZgody,
+                    sum(bazazgZgody) as zgodyZgody,
+                    sum(bazaresztaZgody) as resztaZgody,
+                    sum(bazaeventZgody) as eventZgody,
+                    sum(bazaexitoZgody) as exitoZgody,
+                                        
+                    sum(baza8)+sum(bazazg)+sum(bazareszta)+sum(bazaevent)+sum(bazaexito)
+                     + sum(baza8Zgody)+sum(bazazgZgody)+sum(bazaresztaZgody)+sum(bazaeventZgody)+sum(bazaexitoZgody) as suma')
+            ->join('users_t', 'id_user', 'users_t.id')
+            ->join('departments_t', 'users_t.dep_id', 'departments_t.id');
+
         if($typ == 1) {
-            $departamentres = DB::table('log_download')
-                ->selectRaw('departments_t.name,departments_t.id,sum(baza8) as bisnode,sum(bazazg) as zgody,sum(bazareszta) as reszta,sum(bazaevent) as event,sum(bazaexito) as exito,sum(baza8)+sum(bazazg)+sum(bazareszta)+sum(bazaevent)+sum(bazaexito) as suma')
-                ->join('users_t', 'id_user', 'users_t.id')
-                ->join('departments_t', 'users_t.dep_id', 'departments_t.id')
+            $departamentres = $departamentres
                 ->where('date', 'like', $date[0] . '%')
                 ->groupBy('departments_t.name')
                 ->groupBy('departments_t.id')
                 ->get();
         }else
         {
-            $departamentres = DB::table('log_download')
-                ->selectRaw('departments_t.name,departments_t.id,sum(baza8) as bisnode,sum(bazazg) as zgody,sum(bazareszta) as reszta,sum(bazaevent) as event,sum(bazaexito) as exito,sum(baza8)+sum(bazazg)+sum(bazareszta)+sum(bazaevent)+sum(bazaexito) as suma')
-                ->join('users_t', 'id_user', 'users_t.id')
-                ->join('departments_t', 'users_t.dep_id', 'departments_t.id')
+            $departamentres = $departamentres
                 ->whereBetween('date', [$date[0] . '%', $date[1] . ' 23:59:59'])
                 ->groupBy('departments_t.name')
                 ->groupBy('departments_t.id')
@@ -636,10 +665,29 @@ class RaportsController extends Controller
 //        $departamentship = $this->setArray($departamentship);
 
 ///////////////////////////////////////Badania Pracownicy/////////////////////////////////////////////
+        $employeeres = DB::table('log_download')
+            ->selectRaw('
+                users_t.name,
+                users_t.last,
+                users_t.id, 
+                users_t.dep_id,
+                    sum(baza8) as bisnode,
+                    sum(bazazg) as zgody,
+                    sum(bazareszta) as reszta,
+                    sum(bazaevent) as event,
+                    sum(bazaexito) as exito,
+                    
+                    sum(baza8Zgody) as bisnodeZgody,
+                    sum(bazazgZgody) as zgodyZgody,
+                    sum(bazaresztaZgody) as resztaZgody,
+                    sum(bazaeventZgody) as eventZgody,
+                    sum(bazaexitoZgody) as exitoZgody,
+                                        
+                    sum(baza8)+sum(bazazg)+sum(bazareszta)+sum(bazaevent)+sum(bazaexito)
+                     + sum(baza8Zgody)+sum(bazazgZgody)+sum(bazaresztaZgody)+sum(bazaeventZgody)+sum(bazaexitoZgody) as suma')
+            ->join('users_t', 'id_user', 'users_t.id');
         if($typ == 1) {
-            $employeeres = DB::table('log_download')
-                ->selectRaw('users_t.name,users_t.last,users_t.id, users_t.dep_id, sum(baza8) as bisnode,sum(bazazg) as zgody,sum(bazareszta) as reszta,sum(bazaevent) as event,sum(bazaexito) as exito,sum(baza8)+sum(bazazg)+sum(bazareszta)+sum(bazaevent)+sum(bazaexito) as suma')
-                ->join('users_t', 'id_user', 'users_t.id')
+            $employeeres = $employeeres
                 ->where('date', 'like', $date[0] . '%')
                 ->groupBy('users_t.name')
                 ->groupBy('users_t.last')
@@ -649,9 +697,7 @@ class RaportsController extends Controller
         }
         else
         {
-            $employeeres = DB::table('log_download')
-                ->selectRaw('users_t.name,users_t.last,users_t.id, users_t.dep_id, sum(baza8) as bisnode,sum(bazazg) as zgody,sum(bazareszta) as reszta,sum(bazaevent) as event,sum(bazaexito) as exito,sum(baza8)+sum(bazazg)+sum(bazareszta)+sum(bazaevent)+sum(bazaexito) as suma')
-                ->join('users_t', 'id_user', 'users_t.id')
+            $employeeres = $employeeres
                 ->whereBetween('date', [$date[0] . '%', $date[1] . ' 23:59:59'])
                 ->groupBy('users_t.name')
                 ->groupBy('users_t.last')
@@ -690,18 +736,34 @@ class RaportsController extends Controller
 //        $employeeship = json_decode(json_encode((array) $employeeship), true);
 //        $employeeship = $this->setArray($employeeship);
 
+        $cityres = DB::table('log_download')
+            ->selectRaw('
+                woj.woj,
+                miasto,
+                id_user,sum(baza8) as bisnode,
+                    sum(baza8) as bisnode,
+                    sum(bazazg) as zgody,
+                    sum(bazareszta) as reszta,
+                    sum(bazaevent) as event,
+                    sum(bazaexito) as exito,
+                    
+                    sum(baza8Zgody) as bisnodeZgody,
+                    sum(bazazgZgody) as zgodyZgody,
+                    sum(bazaresztaZgody) as resztaZgody,
+                    sum(bazaeventZgody) as eventZgody,
+                    sum(bazaexitoZgody) as exitoZgody,
+                                        
+                    sum(baza8)+sum(bazazg)+sum(bazareszta)+sum(bazaevent)+sum(bazaexito)
+                     + sum(baza8Zgody)+sum(bazazgZgody)+sum(bazaresztaZgody)+sum(bazaeventZgody)+sum(bazaexitoZgody) as suma')
+            ->join('woj', 'log_download.idwoj', 'woj.idwoj');
         if($typ == 1) {
-            $cityres = DB::table('log_download')
-                ->selectRaw('woj.woj,miasto,id_user,sum(baza8) as bisnode,sum(bazazg) as zgody,sum(bazareszta) as reszta,sum(bazaevent) as event,sum(bazaexito) as exito,sum(baza8)+sum(bazazg)+sum(bazareszta)+sum(bazaevent)+sum(bazaexito) as suma')
-                ->join('woj', 'log_download.idwoj', 'woj.idwoj')
+            $cityres = $cityres
                 ->where('date', 'like', $date[0] . '%')
                 ->groupBy('miasto', 'id_user', 'woj.woj')
                 ->get();
 
         }else {
-            $cityres = DB::table('log_download')
-                ->selectRaw('woj.woj,miasto,id_user,sum(baza8) as bisnode,sum(bazazg) as zgody,sum(bazareszta) as reszta,sum(bazaevent) as event,sum(bazaexito) as exito,sum(baza8)+sum(bazazg)+sum(bazareszta)+sum(bazaevent)+sum(bazaexito) as suma')
-                ->join('woj', 'log_download.idwoj', 'woj.idwoj')
+            $cityres = $cityres
                 -> whereBetween('date', [$date[0] . '%', $date[1] . ' 23:59:59'])
                 ->groupBy('miasto', 'id_user', 'woj.woj')
                 ->get();
--- resources/views/raports/raportplusPOST.blade.php
+++ resources/views/raports/raportplusPOST.blade.php
@@ -114,17 +114,22 @@
 
     <table class="table table-bordered">
         <thead>
-        <tr style="background: yellow">
-            <th>NR</th>
-            <th>Imie</th>
-            <th>Nazwisko</th>
-            <th>Bisnode</th>
-            <th>Zgody</th>
-            <th>Event</th>
-            <th>Reszta</th>
-            <th>Exito</th>
-            <th>Suma</th>
-        </tr>
+            <tr style="background: yellow">
+                <th>NR</th>
+                <th>Imie</th>
+                <th>Nazwisko</th>
+                <th>Bisnode</th>
+                <th>Bisnode Zgody</th>
+                <th>Zgody</th>
+                <th>Zgody Nowe</th>
+                <th>Event</th>
+                <th>Event Zgody</th>
+                <th>Reszta</th>
+                <th>Reszta Zgody</th>
+                <th>Exito</th>
+                <th>Exito Zgody</th>
+                <th>Suma</th>
+            </tr>
         </thead>
         <tbody>
 
@@ -132,11 +137,16 @@
         <?php if(!empty($dane[0])) :?>
         <tr class="badania">
             <td colspan="3" style="text-align: center"><b>Ogół</b></td>
-            <td><b>{{number_format($dane[0]['bisnode']/$dane[0]['suma'], 2, '', '')+0 }}%</b></td>
-            <td><b>{{number_format($dane[0]['zgody']/$dane[0]['suma'], 2, '', '')+0 }}%</b></td>
-            <td><b>{{number_format($dane[0]['event']/$dane[0]['suma'], 2, '', '')+0 }}%</b></td>
-            <td><b>{{number_format($dane[0]['reszta']/$dane[0]['suma'], 2, '', '')+0 }}%</b></td>
-            <td><b>{{number_format($dane[0]['exito']/$dane[0]['suma'], 2, '', '')+0 }}%</b></td>
+                <td><b>{{number_format($dane[0]['bisnode']/$dane[0]['suma'], 2, '', '')+0 }}%</b></td>
+                <td><b>{{number_format($dane[0]['bisnodeZgody']/$dane[0]['suma'], 2, '', '')+0 }}%</b></td>
+                <td><b>{{number_format($dane[0]['zgody']/$dane[0]['suma'], 2, '', '')+0 }}%</b></td>
+                <td><b>{{number_format($dane[0]['zgodyZgody']/$dane[0]['suma'], 2, '', '')+0 }}%</b></td>
+                <td><b>{{number_format($dane[0]['event']/$dane[0]['suma'], 2, '', '')+0 }}%</b></td>
+                <td><b>{{number_format($dane[0]['eventZgody']/$dane[0]['suma'], 2, '', '')+0 }}%</b></td>
+                <td><b>{{number_format($dane[0]['reszta']/$dane[0]['suma'], 2, '', '')+0 }}%</b></td>
+                <td><b>{{number_format($dane[0]['resztaZgody']/$dane[0]['suma'], 2, '', '')+0 }}%</b></td>
+                <td><b>{{number_format($dane[0]['exito']/$dane[0]['suma'], 2, '', '')+0 }}%</b></td>
+                <td><b>{{number_format($dane[0]['exitoZgody']/$dane[0]['suma'], 2, '', '')+0 }}%</b></td>
             <td><b>{{$dane[0]['suma']}}</b></td>
         </tr>
 
@@ -144,12 +154,16 @@
 
         <tr class="badania">
             <td colspan="3"><b> <?php echo $item['name']; ?> </b></td>
-            <td><b>{{number_format($item['bisnode']/$item['suma'], 2, '', '')+0 }}%</b> </td>
-            <td><b> {{number_format($item['zgody']/$item['suma'], 2, '', '')+0 }}% </b></td>
-            <td><b> {{number_format($item['event']/$item['suma'], 2, '', '')+0 }}% </b></td>
-            <td><b> {{number_format($item['reszta']/$item['suma'], 2, '', '')+0 }}%</b> </td>
-            <td><b> {{number_format($item['exito']/$item['suma'], 2, '', '')+0 }}%</b> </td>
-
+            <td><b>{{number_format($item['bisnode']/$item['suma'], 2, '', '')+0 }}%</b></td>
+            <td><b>{{number_format($item['bisnodeZgody']/$item['suma'], 2, '', '')+0 }}%</b></td>
+            <td><b>{{number_format($item['zgody']/$item['suma'], 2, '', '')+0 }}%</b></td>
+            <td><b>{{number_format($item['zgodyZgody']/$item['suma'], 2, '', '')+0 }}%</b></td>
+            <td><b>{{number_format($item['event']/$item['suma'], 2, '', '')+0 }}%</b></td>
+            <td><b>{{number_format($item['eventZgody']/$item['suma'], 2, '', '')+0 }}%</b></td>
+            <td><b>{{number_format($item['reszta']/$item['suma'], 2, '', '')+0 }}%</b></td>
+            <td><b>{{number_format($item['resztaZgody']/$item['suma'], 2, '', '')+0 }}%</b></td>
+            <td><b>{{number_format($item['exito']/$item['suma'], 2, '', '')+0 }}%</b></td>
+            <td><b>{{number_format($item['exitoZgody']/$item['suma'], 2, '', '')+0 }}%</b></td>
             <td><b> {{ $item['suma'] }} </b></td>
         </tr>
         <?php foreach ($employeeres as $value):?>
@@ -159,12 +173,16 @@
                 <td> {{ $i++ }}</td>
                 <td> {{ $value['name'] }}</td>
                 <td> {{ $value['last'] }}</td>
-                <td> {{number_format($value['bisnode']/$value['suma'], 2, '', '')+0 }}% </td>
-                <td> {{number_format($value['zgody']/$value['suma'], 2, '', '')+0 }}% </td>
-                <td> {{number_format($value['event']/$value['suma'], 2, '', '')+0 }}% </td>
-                <td> {{number_format($value['reszta']/$value['suma'], 2, '', '')+0 }}% </td>
-                <td> {{number_format($value['exito']/$value['suma'], 2, '', '')+0 }}% </td>
-
+                <td><b>{{number_format($value['bisnode']/$value['suma'], 2, '', '')+0 }}%</b></td>
+                <td><b>{{number_format($value['bisnodeZgody']/$value['suma'], 2, '', '')+0 }}%</b></td>
+                <td><b>{{number_format($value['zgody']/$value['suma'], 2, '', '')+0 }}%</b></td>
+                <td><b>{{number_format($value['zgodyZgody']/$value['suma'], 2, '', '')+0 }}%</b></td>
+                <td><b>{{number_format($value['event']/$value['suma'], 2, '', '')+0 }}%</b></td>
+                <td><b>{{number_format($value['eventZgody']/$value['suma'], 2, '', '')+0 }}%</b></td>
+                <td><b>{{number_format($value['reszta']/$value['suma'], 2, '', '')+0 }}%</b></td>
+                <td><b>{{number_format($value['resztaZgody']/$value['suma'], 2, '', '')+0 }}%</b></td>
+                <td><b>{{number_format($value['exito']/$value['suma'], 2, '', '')+0 }}%</b></td>
+                <td><b>{{number_format($value['exitoZgody']/$value['suma'], 2, '', '')+0 }}%</b></td>
                 <td> {{ $value['suma'] }} </td>
             </tr>
             <?php foreach ($cityres as  $key):?>
@@ -174,11 +192,15 @@
                     <td> {{ $key['woj'] }}</td>
                     <td> {{ $key['miasto'] }}</td>
                     <td> {{ $key['bisnode'] }} </td>
+                    <td> {{ $key['bisnodeZgody'] }} </td>
                     <td> {{ $key['zgody'] }} </td>
+                    <td> {{ $key['zgodyZgody'] }} </td>
                     <td> {{ $key['event'] }} </td>
+                    <td> {{ $key['eventZgody'] }} </td>
                     <td> {{ $key['reszta'] }} </td>
+                    <td> {{ $key['resztaZgody'] }} </td>
                     <td> {{ $key['exito'] }} </td>
-
+                    <td> {{ $key['exitoZgody'] }} </td>
                     <td> {{ $key['suma'] }} </td>
                 </tr>
             @endif
