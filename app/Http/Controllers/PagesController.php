f98c49943b86906ea042bb2a439f72bcf6cd21ad
spik8 <skobry123on@gmail.com>
2018-05-09 09:45:58 +0200

    delete fake database

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -637,7 +637,7 @@ class PagesController extends Controller
 
             if($typ == 0) {
                 $rekody = $rekody
-                    ->where('idbaza', '=', 8,38);
+                    ->where('idbaza', '=', 8);
             }else if($typ == 1)
             {
                 $rekody = $rekody
